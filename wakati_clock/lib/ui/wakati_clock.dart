850bae160352f82728b1830c2f618be953d06cf4
kilasi <joseph.kilasi@incentro.com>
2019-12-27 21:43:25 +0300

    decided to use timer approach clock now updates

--- wakati_clock/lib/ui/wakati_clock.dart
+++ wakati_clock/lib/ui/wakati_clock.dart
@@ -1,9 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'package:flutter_clock_helper/model.dart';
 
 class WakatiClock extends StatefulWidget {
- 
   const WakatiClock(this.model);
 
   final ClockModel model;
@@ -11,10 +12,34 @@ class WakatiClock extends StatefulWidget {
 }
 
 class _WakatiState extends State<WakatiClock> {
+  Timer _timer;
+  DateTime dateTime;
+  bool is24 = ClockModel().is24HourFormat;
+
+  @override
+  void initState() {
+    super.initState();
+    dateTime = new DateTime.now();
+    _timer = new Timer.periodic(const Duration(seconds: 1), setTime);
+  }
+
+   void setTime(Timer timer) {
+    setState(() {
+      dateTime = new DateTime.now();
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Container(),
+      child: Container(
+        child:Text("The time is ${dateTime.hour} : ${dateTime.minute} : ${dateTime.second}")),
     );
   }
 }

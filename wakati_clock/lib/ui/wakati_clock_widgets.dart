ec13e361edb64b2880a0b9b12d6c23b8104f15f5
kilasi <joseph.kilasi@incentro.com>
2020-01-18 19:32:15 +0300

    added new clock faces

---
+++ wakati_clock/lib/ui/wakati_clock_widgets.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:wakati_clock/utils/strings.dart';
+
+// Used to build the center widget with the time
+Center buildTimeWidget(String formattedHour, String formattedMinute) {
+  return Center(
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          "$formattedHour",
+          style: TextStyle(fontSize: 100, fontFamily: primaryFont),
+        ),
+        Text(
+          ":",
+          style: TextStyle(fontSize: 50, fontFamily: primaryFont),
+        ),
+        Text(
+          "$formattedMinute",
+          style: TextStyle(fontSize: 50, fontFamily: primaryFont),
+        ),
+      ],
+    ),
+  );
+}
+
+// Builds the date section on the top right
+Positioned buildDateSection(DateTime theTimeNow,Color color) {
+  var today = theTimeNow != null ? DateFormat.E().format(theTimeNow) : '';
+  var dayOfTheWeek =
+      theTimeNow != null ? DateFormat.d().format(theTimeNow) : 'Today';
+  return Positioned(
+    right: 0,
+    child: ClipRRect(
+      borderRadius: BorderRadius.circular(4.0),
+      child: Opacity(
+        opacity: 0.6,
+        child: Container(
+          height: 110,
+          color: color,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                Text("$today",
+                    style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: secondaryFont)),
+                Text("$dayOfTheWeek",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: primaryFont)),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+// Builds the location section on the bottom left
+Widget buildLocationSection(String currentLocation) {
+  return Positioned(
+    bottom: 0,
+    right: 0,
+    child: Container(
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 15, right: 15),
+        child: Text(
+          "$currentLocation",
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              fontFamily: primaryFont),
+        ),
+      ),
+    ),
+  );
+}
+
+// Builds the weather section
+Widget buildWeatherSection(
+    double deviceWidth, double deviceHeight, num temparature,String unitString) {
+  var formattedTemp = temparature.truncate();
+  return Positioned(
+    bottom: 0,
+    child: Padding(
+      padding: const EdgeInsets.only(left: 5, bottom: 15),
+      child: Container(
+        width: deviceWidth / 3,
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Text(
+                    "$formattedTemp",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35,
+                        fontFamily: primaryFont),
+                  ),
+                ),
+                Text(
+                  "$unitString",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      fontFamily: primaryFont),
+                )
+              ],
+            ),
+            Text(
+              "$weatherPrefix ${formattedTemp + 4} $unitString",
+              style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: secondaryFont,
+                  fontWeight: FontWeight.w700),
+            )
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+// Background for bottom items
+Widget buildBottomContainer(double deviceHeight, double deviceWidth,Color color) {
+  return Positioned(
+      bottom: 0,
+      child: ClipRRect(
+        child: Opacity(
+          opacity: 0.4,
+          child: Container(
+            height: deviceHeight / 4,
+            width: deviceWidth,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0.1, 0.5],
+                    colors: [Colors.transparent, color])),
+          ),
+        ),
+      ));
+}
--- wakati_clock/lib/utils/strings.dart
+++ wakati_clock/lib/utils/strings.dart
@@ -1,11 +1,13 @@
-/// Contains all string to be used in the app
+/// Contains all string to be used in the app and references to images
 
-const String sunnyBackground = 'assets/images/sunnyBackground.png';
+const String sunnyBackground = 'assets/images/Wakati_clock_face1.png';
 const String sunnyBackground2 = 'assets/images/sunnyBackground2.png';
-const String windyBackground = 'assets/images/nightCloudyBackground.png';
+const String nightCloudyBackground = 'assets/images/nightCloudyBackground.png';
 const String rainyBackground = 'assets/images/rainBackground.png';
-const String nightBackground = 'assets/images/nightCloudyBackground.png';
-const String nightBackground2 = 'assets/images/v4.png';
+const String cloudyBackground = 'assets/images/Wakati_clock_face5.png';
+const String nightBackground = 'assets/images/Wakati_clock_face3.png';
+const String neutralBackground = 'assets/images/Wakati_clock_face4.png';
+const String foggyBackground = 'assets/images/Wakati_clock_face2.png';
 const String primaryFont = 'MeriendaOne';
 const String secondaryFont = 'UbuntuMono';
 const String weatherPrefix = 'Probably feels like';
